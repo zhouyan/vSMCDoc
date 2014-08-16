ed0bb2a16dd8bd0c2bb47d674c9da316e0259bfc
Yan Zhou <zhouyan@me.com>
2014-08-16 16:28:41 +0800

    update doc

--- master/search/files_b.js
+++ master/search/files_b.js
@@ -3,6 +3,7 @@ var searchData=
   ['parallel_5faccumulate_2ehpp',['parallel_accumulate.hpp',['../parallel__accumulate_8hpp.html',1,'']]],
   ['parallel_5ffor_2ehpp',['parallel_for.hpp',['../parallel__for_8hpp.html',1,'']]],
   ['parallel_5freduce_2ehpp',['parallel_reduce.hpp',['../parallel__reduce_8hpp.html',1,'']]],
+  ['parallel_5frepeat_2ehpp',['parallel_repeat.hpp',['../parallel__repeat_8hpp.html',1,'']]],
   ['particle_2ehpp',['particle.hpp',['../particle_8hpp.html',1,'']]],
   ['path_2ehpp',['path.hpp',['../path_8hpp.html',1,'']]],
   ['philox_2ehpp',['philox.hpp',['../philox_8hpp.html',1,'']]],
